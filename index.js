1b93e204e8b16ea84062e54ef4d7eb85e24d9f8f
JanjanC <janjan_chua@dlsu.edu.ph>
2023-06-20 23:06:22 +0800

    Added dynamic loading of percentage based on user_id

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 const express = require('express');
+const hbs = require('hbs');
 const session = require('express-session');
 const bodyParser = require(`body-parser`);
 const path = require('path');
@@ -27,8 +28,11 @@ app.use(
         },
     })
 );
-
 app.use(express.static(path.join(__dirname, 'assets')));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+console.log(path.join(__dirname, 'views'));
 
 // Landing Page
 app.get('/', function (req, res) {
@@ -82,11 +86,6 @@ app.get('/signup/rentee', function (req, res) {
     res.sendFile(path.join(__dirname, 'views', 'signup-rentee.html'));
 });
 
-app.get('/signup/rentee/parking', function (req, res) {
-    res.sendFile(path.join(__dirname, 'views', 'signup-parking-space.html'));
-});
-
-// POST Requests
 app.post('/signup/rentee', function (req, res) {
     // save user event
     let query = 'INSERT INTO logs (ip_address, event) VALUES (?, ?)';
@@ -105,17 +104,44 @@ app.post('/signup/rentee', function (req, res) {
     });
 });
 
+app.get('/signup/rentee/parking', function (req, res) {
+    const percent = [3, 5, 7];
+    console.log('SESSION ID');
+    console.log(req.session.user_id);
+    console.log(req.session.user_id.slice(-1));
+    index = parseInt(req.session.user_id.slice(-1), 16) % 3;
+
+    res.render('signup-parking-space', { percent: percent[index] });
+});
+
+app.post('/signup/rentee/parking', function (req, res) {
+    console.log(req.body);
+    query = 'INSERT INTO parking_info (user_id, ip_address, number, location, time_start, time_end, price) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    const UUID = crypto.randomUUID();
+    params = [UUID, req.clientIp, req.body.name, req.body.birthdate, req.body.email, req.body.contact, 'Parking Owner'];
+    connection.execute(query, params, function (err, results) {
+        console.log(results);
+        req.session.user_id = UUID;
+        res.redirect('/success');
+    });
+});
+
 //Sucess and Error Pages
 app.get('/success', function (req, res) {
     if (req.session.user_id) {
+        req.session.destroy();
         res.sendFile(path.join(__dirname, 'views', 'success.html'));
     } else {
-        res.redirect('error');
+        res.redirect('/error');
     }
 });
 
 app.get('/error', function (req, res) {
     res.sendFile(path.join(__dirname, 'views', 'error.html'));
+});
+
+app.get('*', function (req, res) {
+    res.redirect('/error');
 });
 
 let listener = app.listen(process.env.PORT || 3000, function () {
