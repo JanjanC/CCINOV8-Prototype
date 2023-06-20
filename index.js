6925e14f2109f6da19085c8c7371f00f644647a8
JanjanC <janjan_chua@dlsu.edu.ph>
2023-06-20 21:58:21 +0800

    Added sessions and user ids

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const dotenv = require('dotenv');
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require(`body-parser`);
 const path = require('path');
 const mysql = require('mysql2');
 const requestIp = require('request-ip');
+const crypto = require('crypto');
+const mysqlStore = require('express-mysql-session')(session);
 
 dotenv.config();
+
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+const sessionStore = new mysqlStore({} /* session store options */, connection);
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(requestIp.mw());
-
-const connection = mysql.createConnection(process.env.DATABASE_URL);
+app.use(
+    session({
+        resave: false,
+        saveUninitialized: false,
+        store: sessionStore,
+        secret: process.env.SESSION_SECRET,
+        cookie: {
+            maxAge: 1 * 60 * 60 * 1000,
+        },
+    })
+);
 
 app.use(express.static(path.join(__dirname, 'assets')));
 
@@ -66,10 +81,13 @@ app.post('/signup/rentee', function (req, res) {
         console.log(results);
     });
 
-    query = 'INSERT INTO registration (ip_address, name, birthdate, email, contact, type) VALUES (?, ?, ?, ?, ?, ?)';
-    params = [req.clientIp, req.body.name, req.body.birthdate, req.body.email, req.body.contact, 'Parking Owner'];
+    query = 'INSERT INTO registration (user_id, ip_address, name, birthdate, email, contact, type) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    const UUID = crypto.randomUUID();
+    params = [UUID, req.clientIp, req.body.name, req.body.birthdate, req.body.email, req.body.contact, 'Parking Owner'];
     connection.execute(query, params, function (err, results) {
-        res.redirect('/signup/rentee/parking/details');
+        console.log(results);
+        req.session.user_id = UUID;
+        res.redirect('/signup/rentee/parking/');
     });
 });
 
@@ -80,9 +98,11 @@ app.post('/signup/renter', function (req, res) {
         console.log(results);
     });
 
-    query = 'INSERT INTO registration (ip_address, name, birthdate, email, contact, type) VALUES (?, ?, ?, ?, ?, ?)';
-    params = [req.clientIp, req.body.name, req.body.birthdate, req.body.email, req.body.contact, 'Driver'];
+    query = 'INSERT INTO registration (user_id, ip_address, name, birthdate, email, contact, type) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    const UUID = crypto.randomUUID();
+    params = [UUID, req.clientIp, req.body.name, req.body.birthdate, req.body.email, req.body.contact, 'Driver'];
     connection.execute(query, params, function (err, results) {
+        req.session.user_id = UUID;
         res.redirect('/success');
     });
 });
