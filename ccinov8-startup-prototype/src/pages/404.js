d2ed4ba070b946e320190c3b1ad3a997387f9cee
JanjanC <janjan_chua@dlsu.edu.ph>
2023-07-11 19:09:25 +0800

    feat: add 404 page

---
+++ ccinov8-startup-prototype/src/pages/404.js
@@ -0,0 +1,16 @@
+const Error = () => {
+    return (
+        <div class="container h-100">
+            <div class="row h-100 justify-content-center align-items-center">
+                <div class="text-center">
+                    <h4>PAGE NOT FOUND</h4>
+                    <a href="/" class="btn btn-primary">
+                        RETURN HOME
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Error;
