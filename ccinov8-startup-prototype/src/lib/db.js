c41765015b7972144a62c3dffbebdfd4355d3bb9
JanjanC <janjan_chua@dlsu.edu.ph>
2023-07-12 03:20:29 +0800

    feat: established connection to database

---
+++ ccinov8-startup-prototype/src/lib/db.js
@@ -0,0 +1,23 @@
+import mysql from 'serverless-mysql';
+const db = mysql({
+    config: {
+        host: process.env.DB_HOSTNAME,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_SCHEMA,
+        ssl: {
+            rejectUnauthorized: true,
+        },
+    },
+});
+
+export default async function excuteQuery({ query, values }) {
+    try {
+        const results = await db.query(query, values);
+        await db.end();
+        return results;
+    } catch (error) {
+        return { error };
+    }
+}
