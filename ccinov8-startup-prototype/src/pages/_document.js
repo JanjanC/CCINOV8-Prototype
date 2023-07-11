9c44ad58c4f3dd8452e148c7b5161ba7b9e7dd68
JanjanC <janjan_chua@dlsu.edu.ph>
2023-07-11 16:58:18 +0800

    refactor: moved navbar from layout to _document

--- ccinov8-startup-prototype/src/pages/_document.js
+++ ccinov8-startup-prototype/src/pages/_document.js
@@ -1,3 +1,4 @@
+import Navbar from '@/components/Navbar';
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
@@ -9,6 +10,7 @@ export default function Document() {
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
             </Head>
             <body>
+                <Navbar />
                 <Main />
                 <NextScript />
             </body>
