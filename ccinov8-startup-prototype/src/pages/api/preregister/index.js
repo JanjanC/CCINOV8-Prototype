cba591e1b2503605f230b74df4b5110711b3e7c2
JanjanC <janjan_chua@dlsu.edu.ph>
2023-07-12 17:43:42 +0800

    feat: added checking for post request in preregister api

--- ccinov8-startup-prototype/src/pages/api/preregister/index.js
+++ ccinov8-startup-prototype/src/pages/api/preregister/index.js
@@ -3,17 +3,21 @@ import requestIp from 'request-ip';
 import { randomUUID } from 'crypto';
 
 export default async (req, res) => {
-    try {
-        const UUID = randomUUID();
-        const ipAddress = requestIp.getClientIp(req);
-        const user = JSON.parse(req.body);
-        const result = await executeQuery({
-            query: 'INSERT INTO registration (user_id, ip_address, name, birthdate, email, contact, type) VALUES (?, ?, ?, ?, ?, ?, ?)',
-            values: [UUID, ipAddress, user.name, user.birthdate, user.email, user.contact, user.type],
-        });
-        res.status(201).json(result);
-        return res.json(result);
-    } catch (error) {
-        res.status(500).send(error.message);
+    switch (req.method) {
+        case 'POST':
+            try {
+                const UUID = randomUUID();
+                const ipAddress = requestIp.getClientIp(req);
+                const user = JSON.parse(req.body);
+                const result = await executeQuery({
+                    query: 'INSERT INTO registration (user_id, ip_address, name, birthdate, email, contact, type) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                    values: [UUID, ipAddress, user.name, user.birthdate, user.email, user.contact, user.type],
+                });
+                res.status(201).json(result);
+                return res.json(result);
+            } catch (error) {
+                res.status(500).send(error.message);
+            }
+            break;
     }
 };
