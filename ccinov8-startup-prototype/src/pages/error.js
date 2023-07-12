3dc8fb6e10d5dbd5fc7591c12a744e4a9944037f
JanjanC <janjan_chua@dlsu.edu.ph>
2023-07-12 19:13:50 +0800

    feat: create error page

---
+++ ccinov8-startup-prototype/src/pages/error.js
@@ -0,0 +1,16 @@
+const Error = () => {
+    return (
+        <div className="container h-100">
+            <div className="row h-100 justify-content-center align-items-center">
+                <div className="text-center">
+                    <h4>SOMETHING WENT WRONG</h4>
+                    <a href="/" className="btn btn-primary">
+                        RETURN HOME
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Error;
