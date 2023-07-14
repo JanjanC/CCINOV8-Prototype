92ab01b8858a5bcfacfa3afd1514135b812edee9
JanjanC <janjan_chua@dlsu.edu.ph>
2023-07-15 02:25:42 +0800

    feat: used cards for the owner home page

--- ccinov8-startup-prototype/src/pages/owner/index.js
+++ ccinov8-startup-prototype/src/pages/owner/index.js
@@ -1,9 +1,11 @@
+import Image from 'next/image';
+
 const OwnerHome = () => {
     return (
         <>
-            <h1>Welcome Back Owner!</h1>
+            {/* <h1>Welcome Back Owner!</h1> */}
 
-            <div className="Notif">
+            {/* <div classNameName="Notif">
                 <h2>Notifications</h2>
 
                 <ul>
@@ -11,37 +13,37 @@ const OwnerHome = () => {
                     <h3>Alice has booked your parking - Pedro Gil</h3>
                     <h3>Charlie has booked your parking - EGI Taft Towers</h3>
                 </ul>
-            </div>
-            <section>
-                <div className="Parking">
-                    <div className="Spot1">
-                        <img
-                            src="https://parklio.com/assets/img/blog/100003/the-best-solution-for-parking-protection_1657283968214.jpg"
-                            alt="Parking Spot 1"
-                        ></img>
-                        <h3>Taft Avenue</h3>
-                        <p>OCCUPIED</p>
-                    </div>
+            </div> */}
 
-                    <div className="Spot2">
-                        <img
-                            src="https://hips.hearstapps.com/hmg-prod/images/vacant-car-parking-space-royalty-free-image-1591721505.jpg"
-                            alt="Parking Spot 1"
-                        ></img>
-                        <h3>Pedro Gil</h3>
-                        <p>UNOCCUPIED</p>
-                    </div>
-
-                    <div className="Spot3">
-                        <img
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Blue_Disc_Parking_Area_Markings_Blue_Paint.JPG/1200px-Blue_Disc_Parking_Area_Markings_Blue_Paint.JPG"
-                            alt="Parking Spot 1"
-                        ></img>
-                        <h3>EGI Taft Towers</h3>
-                        <p>BOOKED</p>
+            <div className="row">
+                <div className="card mb-4">
+                    <Image
+                        className="card-img-top img-fluid"
+                        src={'/images/drivers.png'}
+                        width={320}
+                        height={180}
+                        alt="Card image cap"
+                    ></Image>
+                    <div className="card-body">
+                        <h4 className="card-title">Taft Avenue</h4>
+                        <p className="card-text">Occupied</p>
                     </div>
                 </div>
-            </section>
+
+                <div className="card mb-4">
+                    <Image
+                        className="card-img-top img-fluid"
+                        src={'/images/drivers.png'}
+                        width={320}
+                        height={180}
+                        alt="Card image cap"
+                    ></Image>
+                    <div className="card-body">
+                        <h4 className="card-title">Taft Avenue</h4>
+                        <p className="card-text">Occupied</p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
