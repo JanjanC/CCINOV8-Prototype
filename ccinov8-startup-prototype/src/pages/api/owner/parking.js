b404c4005ec9d99f138f3d6636db88ada09bbf3e
JanjanC <janjan_chua@dlsu.edu.ph>
2023-07-16 17:38:48 +0800

    feat: added server side rendering for list of parking spaces

---
+++ ccinov8-startup-prototype/src/pages/api/owner/parking.js
@@ -0,0 +1,17 @@
+import executeQuery from '../../../lib/db';
+
+export default async (req, res) => {
+    switch (req.method) {
+        case 'GET':
+            try {
+                const result = await executeQuery({
+                    query: 'SELECT * FROM parking',
+                });
+
+                return res.status(200).json(result);
+            } catch (error) {
+                return res.status(500).json({ message: error.message });
+            }
+            break;
+    }
+};
--- ccinov8-startup-prototype/src/pages/owner/index.js
+++ ccinov8-startup-prototype/src/pages/owner/index.js
@@ -1,6 +1,16 @@
 import Image from 'next/image';
+import { useEffect } from 'react';
 
-const OwnerHome = () => {
+export const getServerSideProps = async () => {
+    const res = await fetch(process.env.BASE_URL + '/api/owner/parking');
+    const data = await res.json();
+
+    return {
+        props: { parkings: data },
+    };
+};
+
+const OwnerHome = ({ parkings }) => {
     return (
         <>
             {/* <h1>Welcome Back Owner!</h1> */}
@@ -16,33 +26,21 @@ const OwnerHome = () => {
             </div> */}
 
             <div className="row">
-                <div className="card mb-4">
-                    <Image
-                        className="card-img-top img-fluid"
-                        src={'/images/drivers.png'}
-                        width={320}
-                        height={180}
-                        alt="Card image cap"
-                    ></Image>
-                    <div className="card-body">
-                        <h4 className="card-title">Taft Avenue</h4>
-                        <p className="card-text">Occupied</p>
+                {parkings.map((parking) => (
+                    <div className="card mb-4">
+                        <Image
+                            className="card-img-top img-fluid"
+                            src={'/images/drivers.png'}
+                            width={320}
+                            height={180}
+                            alt="Card image cap"
+                        ></Image>
+                        <div className="card-body">
+                            <h4 className="card-title">{parking.name}</h4>
+                            <p className="card-text">Occupied</p>
+                        </div>
                     </div>
-                </div>
-
-                <div className="card mb-4">
-                    <Image
-                        className="card-img-top img-fluid"
-                        src={'/images/drivers.png'}
-                        width={320}
-                        height={180}
-                        alt="Card image cap"
-                    ></Image>
-                    <div className="card-body">
-                        <h4 className="card-title">Taft Avenue</h4>
-                        <p className="card-text">Occupied</p>
-                    </div>
-                </div>
+                ))}
             </div>
         </>
     );
