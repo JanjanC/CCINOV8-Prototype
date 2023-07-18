0f5cf0bab4874a42033927a7a421e76c511176b4
iDLEWINK <josh_uson@dlsu.edu.ph>
2023-07-18 16:37:38 +0800

    feat: add hashed ID to each link

--- ccinov8-startup-prototype/src/pages/driver/index.js
+++ ccinov8-startup-prototype/src/pages/driver/index.js
@@ -1,8 +1,15 @@
 import ParkingSpotCard from '@/components/parking-spot-card';
+import Crypto from 'crypto';
 
 export const getServerSideProps = async () => {
     const res = await fetch(process.env.BASE_URL + '/api/owner/parking');
     const data = await res.json();
+
+    for (var idx in data) {
+        let id = data[idx].user_id + data[idx].location + data[idx].time + data[idx].time_start + data[idx].time_end;
+        id = Crypto.createHash('sha256', process.env.SESSION_SECRET).update(id).digest('hex');
+        data[idx].id = id;
+    }
 
     return {
         props: { parkings: data },
@@ -19,6 +26,8 @@ export default function DriverHome({ parkings }) {
                 <div className="row d-flex flex-wrap justify-content-around">
                     {parkings.map((parking) => (
                         <ParkingSpotCard
+                            key={parking.id}
+                            id={parking.id}
                             thumbnail="\images\car-parking.png"
                             location={parking.location}
                             price={parking.price}
