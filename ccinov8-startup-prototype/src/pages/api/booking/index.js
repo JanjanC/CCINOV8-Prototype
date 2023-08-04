20bbdbeb9c8ecf2d9281956aea59f4d98aaa32cf
iDLEWINK <josh_uson@dlsu.edu.ph>
2023-08-04 11:54:29 +0800

    feat: update db calls

--- ccinov8-startup-prototype/src/pages/api/booking/index.js
+++ ccinov8-startup-prototype/src/pages/api/booking/index.js
@@ -1,6 +1,5 @@
-import multer from 'multer';
-import fs from 'fs';
 import executeQuery from '../../../lib/db';
+import { randomUUID } from 'crypto';
 
 export default async (req, res) => {
     switch (req.method) {
@@ -9,11 +8,22 @@ export default async (req, res) => {
                 const result = await executeQuery({
                     query: 'SELECT * FROM booking',
                 });
-                console.log(result);
                 return res.status(200).json(result);
             } catch (error) {
                 return res.status(500).json({ message: error.message });
             }
             break;
+
+        case 'POST':
+            const result = await executeQuery({
+                query: 'INSERT INTO booking (parking_id, user_id, datetime_start, datetime_end, active) VALUES (?, ?, ?, ?, ?)',
+                values: [req.body.parking_id, randomUUID(), req.body.datetime_start, req.body.datetime_end, req.body.active],
+            });
+
+            if (result.affectedRows) {
+                res.status(201).json({ message: 'Insert Successful' });
+            } else {
+                throw Error('Insert Failed');
+            }
     }
 };
