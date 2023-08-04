316024266b330a2153c1c21b2c216c97697960a7
iDLEWINK <josh_uson@dlsu.edu.ph>
2023-08-04 12:50:48 +0800

    fix: change db timezone to pht

--- ccinov8-startup-prototype/src/lib/db.js
+++ ccinov8-startup-prototype/src/lib/db.js
@@ -6,6 +6,7 @@ const db = mysql({
         user: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_SCHEMA,
+        timezone: 'pht',
         ssl: {
             rejectUnauthorized: true,
         },
